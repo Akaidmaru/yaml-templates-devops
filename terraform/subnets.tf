e4e3bd52578fbd78c80c7f81fd4436a426959943
Reddmar J. Quevedo C. <reddmar.quevedo@gmail.com>
2024-11-05 21:27:58 -0500

    feat: load balancer
    Added a load balancer to the project between the 2 Public  Subnets

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -25,7 +25,7 @@ resource "aws_subnet" "public-subnet-1" {
   vpc_id            = aws_vpc.vpc-sala5.id
   cidr_block        = "10.0.3.0/24"
   availability_zone = "us-east-1a"
-  
+
   tags = {
     Name = "Public-Subnet-1"
   }
@@ -36,8 +36,19 @@ resource "aws_subnet" "public-subnet-2" {
   vpc_id            = aws_vpc.vpc-sala5.id
   cidr_block        = "10.0.4.0/24"
   availability_zone = "us-east-1b"
-  
+
   tags = {
     Name = "Public-Subnet-2"
+  }
+}
+resource "aws_lb" "public-subnet-lb" {
+  name               = "public-subnet-lb"
+  load_balancer_type = "application"
+
+  subnets         = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
+  security_groups = [aws_security_group.sala5-sg.id]
+
+  tags = {
+    Environment = "Load Balancer Public Subnets"
   }
 }
